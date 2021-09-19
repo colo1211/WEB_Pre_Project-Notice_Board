7f9b2d9340dea0e8a30fdf295cdeb80afc59a84a
colo1211 <colo1211@naver.com>
2021-09-19 15:18:40 +0900

    새로고침을 해도 유저정보 안날아가는 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
+import axios from 'axios'; 
+
 
 const LandingPage = () => {
+    useEffect(()=>{
+        if(localStorage.getItem('accessToken') !== null){
+           axios.post('api/user/login', localStorage.getItem('accessToken'))
+        .then(() => 
+            console.log('아직 유저정보 살아있음')
+        );
+      }    
+    },[]);
 
-    const isLogin = useSelector((state) => state.user.isLogin); 
-    
     return (
         <div>
             Landing Page
             <p>반갑습니다!</p>
-            
         </div>
     )
 }
