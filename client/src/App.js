dae05d14fbf6ecd27f95d0b3e30a1a94d3541d73
colo1211 <colo1211@naver.com>
2021-09-19 19:26:06 +0900

    HOC 적용

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import NavBar from './components/views/NavBar/NavBar';
 import SignIn from './components/views/SignIn/SignIn';
 import SignUp from './components/views/SignUp/SignUp';
+import Auth from './hoc/Auth'; 
 
 function App() {
 
@@ -12,9 +13,9 @@ function App() {
         <Router>
           <NavBar/>
             <Switch>
-              <Route exact path='/' component={ LandingPage }/>  
-              <Route exact path='/signup' component={ SignUp }/>  
-              <Route exact path='/signin' component={ SignIn }/>  
+              <Route exact path='/' component={ Auth(LandingPage, true) }/>  
+              <Route exact path='/signup' component={ Auth(SignUp, false) }/>  
+              <Route exact path='/signin' component={ Auth(SignIn, false) }/>  
             </Switch>  
         </Router>      
     </div>
