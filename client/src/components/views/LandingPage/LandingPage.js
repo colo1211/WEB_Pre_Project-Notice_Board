785d99c0af7d991f974412abee8a162b57613371
colo1211 <colo1211@naver.com>
2021-09-23 12:06:13 +0900

    Create Form 모듈화

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { loginAction } from '../../../_actions/user_action';
 import { withRouter } from 'react-router';
-import { Button } from 'react-bootstrap'; 
 import './LandingPage.scss';
+import CreatePage from '../CreatePage/CreatePage';
+import { loginAction } from '../../../_actions/user_action';
+import { Button } from 'react-bootstrap'; 
+import ReadPage from '../ReadPage/ReadPage';
 
 const LandingPage = () => {
-
-    const user = useSelector((state) => state); 
+    const user = useSelector((state) => state);     
     const dispatch = useDispatch(); 
+    const [Write, setWrite] = useState(false);
 
     // 로그인 후 새로고침 할때, 유저의 로그인 기록을 유지
     useEffect(()=>{
@@ -25,44 +27,25 @@ const LandingPage = () => {
             .then(() => 
             console.log('유저정보 아직 살아있음')
         );
-      }    
+        }    
     },[]);
 
-
-    const onSubmitHandler = (e) => {
-        e.preventDefault(); 
-        console.log('제출')
+    const WriteButton = () => {
+        setWrite(true); 
     }
+
 
     return (
         <div>
             <p>반갑습니다! {user.user.name}님! </p>
-            <form className='form-layout' >
-                <div className = 'form-wrap'>
-                    <p>제목</p>
-                    <input className= 'input-layout'></input>
-                    <p>내용</p>
-                    <input className= 'input-layout'></input>
-                    <p>장소</p>
-                    <input className= 'input-layout'></input>
-                    <p>학교ID</p>
-                    <input className= 'input-layout'></input>
-                    <p>급여</p>
-                    <input className= 'input-layout'></input>
-                    <p>소요시간</p>
-                    <input className= 'input-layout'></input>
-                    <p>연락처</p>
-                    <input className= 'input-layout' placeholder='010-1234-5678'></input>
-                    <p>카테고리</p>
-                    <input className='input-layout' placeholder='SURVEY or EXPERIMENT'></input>
-                    <p>마감날짜</p>
-                    <input type ='date' className= 'input-layout'></input>
-                    <p>실험/설문날짜</p>
-                    <input className= 'input-layout' placeholder='2021-08-10T09:30'></input>
-                    <Button onSubmit={onSubmitHandler} className = 'mt-5' variant="primary">제출</Button>
-                </div>
-               
-            </form>
+            <ReadPage/>
+            <Button className='primary' onClick = {WriteButton}> 글 생성 </Button> 
+            {
+                Write === true
+                ? <CreatePage setWrite={setWrite}/>
+                : null
+            }           
+
         </div>
     )
 }
