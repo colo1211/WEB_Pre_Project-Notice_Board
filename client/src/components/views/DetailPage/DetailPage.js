de3fc1f9e469baf88e9c6fb6637b7e059b68a1f8
colo1211 <colo1211@naver.com>
2021-09-23 12:07:11 +0900

    Component Rendering vs Axios response Error(비동기 처리)

--- client/src/components/views/DetailPage/DetailPage.js
+++ client/src/components/views/DetailPage/DetailPage.js
@@ -1,33 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router'
+import { useParams,useHistory } from 'react-router'
 import axios from 'axios'; 
+import {Button} from 'react-bootstrap';
 
 const DetailPage = () => {
 
-    const {id} = useParams(); 
+    const history = useHistory(); 
+    const { id } = useParams(); 
     const [DetailContents, setDetailContents] = useState(null); 
+    
     useEffect(()=>{
         let token = JSON.parse(localStorage.getItem('user')).accessToken;
+
         axios.get(`/api/board/${id}`, {
             headers : {
                 Authorization: `Bearer ${token}`
             }
         })
         .then((response)=>{
-            setDetailContents(response.data); 
+            // console.log(`통신 result ${JSON.stringify(response.data)}`)
+            setDetailContents(response.data);
+            console.log(DetailContents); 
         })
-        .catch((e)=>console.log(e)); 
-        console.log(`이건 state: ${JSON.parse(DetailContents)}`);
+        .catch((e)=>console.log(e))
+        .finally(()=>{
+            console.log('기다려')
+        });
     },[]);
 
+
+
     return (
-        <div className='detail-page'>
-            {/* <h2>{DetailContents.title}</h2> */}
-            {/* <p>{DetailContents.author}</p>
-            <p>{DetailContents.doDate}</p>
-            <p>{DetailContents.schoolName}</p>
-            <p>{DetailContents.contact}</p>
-            <p>{DetailContents.place}</p> */}
+        <div className='container'>
+            {
+                DetailContents && 
+                <div>
+                    <h2>{DetailContents.title}</h2> 
+                    <p>작성자 : {DetailContents.author}</p>
+                    <p>실험 일자 : {DetailContents.doDate}</p>
+                    <p>학교 : {DetailContents.schoolName}</p>
+                    <p>연락처 : {DetailContents.contact}</p>
+                    <p>실험 장소 : {DetailContents.place}</p>         
+                </div>
+            }
+            <Button className= 'btn btn-primary' onClick={()=>{history.goBack()}}>뒤로 가기</Button>
         </div>
     )
 }
