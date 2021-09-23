b2004c1aa692cc6cf39350665ec15547ecbaceee
colo1211 <colo1211@naver.com>
2021-09-23 21:46:58 +0900

    Add Create Routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import NavBar from './components/views/NavBar/NavBar';
 import SignIn from './components/views/SignIn/SignIn';
 import SignUp from './components/views/SignUp/SignUp';
+import CreatePage from './components/views/CreatePage/CreatePage';
 import Auth from './hoc/Auth'; 
 
 function App() {
@@ -14,7 +15,8 @@ function App() {
         <Router>
           <NavBar/>
             <Switch>
-              <Route exact path='/' component={ Auth(LandingPage, true) }/>  
+              <Route exact path='/' component={ Auth(LandingPage, true) }/> 
+              <Route exact path='/create' component={ Auth(CreatePage, true) }/> 
               <Route exact path='/signup' component={ Auth(SignUp, false) }/>  
               <Route exact path='/signin' component={ Auth(SignIn, false) }/>  
               <Route exact path='/detail/:id' component={ Auth(DetailPage, true) }/>  
