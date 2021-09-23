87b611a6c6787089b541568f3f3e79be2561608e
colo1211 <colo1211@naver.com>
2021-09-23 18:07:18 +0900

    NavBar 스타일링

--- client/src/components/views/NavBar/NavBar.js
+++ client/src/components/views/NavBar/NavBar.js
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'; 
@@ -6,6 +5,8 @@ import { useHistory } from 'react-router';
 import { logoutAction } from '../../../_actions/user_action';
 
 function NavBar() {
+
+    const isLogin = useSelector((state) => state.user.isLogin); 
     const history = useHistory(); 
     const dispatch = useDispatch(); 
     
@@ -18,16 +19,39 @@ function NavBar() {
             history.push('/signin'); 
         })
         localStorage.removeItem('user'); 
+        localStorage.removeItem('userId'); 
     }
 
     return (
         <div>
-            <ul>
+            {/* <ul>
                 <li><Link to = '/'>Home</Link></li>
                 <li><Link to = '/SignIn'>SignIn</Link></li>
                 <li><Link to = '/SignUp'>SignUp</Link></li>
                 <button onClick={logOutHandler}>Sign Out</button>
-            </ul>
+            </ul> */}
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">Notice Board</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                    
+                        {
+                            isLogin &&
+                            (<ul class="navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page"><Link to = '/'>Home</Link></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" onClick={logOutHandler}> Sign Out </a>
+                                </li>
+                            </ul>)
+                        }
+                    </div>
+                </div>
+            </nav>
         </div>
     )
 }
