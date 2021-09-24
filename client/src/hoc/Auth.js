cc69354295a34c91accc23d5968f07b2e4d647cc
colo1211 <colo1211@naver.com>
2021-09-24 15:34:23 +0900

    로그인/ 이메일,패스워드 정확해야 통과하게 구현

--- client/src/hoc/Auth.js
+++ client/src/hoc/Auth.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
 
 
 export default function (SpecificComponent, option) {
 
     const isLogin = useSelector((state)=> state.user.isLogin);
+    const name = useSelector((state)=> state.user.name);
   
     function AuthCheck(props){
         useEffect(()=>{
@@ -14,7 +14,7 @@ export default function (SpecificComponent, option) {
             // false : 로그인한 유저 출입 불가 페이지
             
             // 로그인 했을 때 : Landing Page만 접속 가능
-            if (isLogin === true){
+            if (isLogin === true && name !== 'Error'){
                 // 로그인한 곳으로 접속하려 하면 라우팅 다른곳으로
                 if (option === false){
                     props.history.push('/'); 
